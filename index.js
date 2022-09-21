2758c37208ec7f41ae5c924abe60a0e4dcf2a568
RycXEpd <2524333609@qq.com>
2022-09-21 15:53:42 +0800

    refactor: use async to keep alive
    BREAKING CHANGE: not use multi-thread mode,may more CPU uses

--- index.js
+++ index.js
@@ -1,67 +1,23 @@
-const constants = {
-    REQUEST_RETRY: 1,
-    NORTHSTAR_RESPAWN: 2,
-    FAILED_AFTER_RETRY: 4,
-    NORTHSTAR_CONSOLE_CLOSED: 8,
-    ALIVE_CHECK: 16
-}
-
-const worker = require("node:worker_threads")
-
-const MasterServerUrl = "https://nscn.wolf109909.top"//Master Server
-const RequestUrl = new URL(MasterServerUrl)
-RequestUrl.pathname = "/client/servers"//API path
+const Server=require('./src/server');
+const Checker=require('./src/checker');
 
 const ServerPaths = [
     "D:\\Program Files (x86)\\Origin Games\\Titanfall2",
     "D:\\Program Files (x86)\\Origin Games\\Titanfall2"
 ]
-const monitors = {}
+const servers = []
 
-function CreateWorker(server, retry) {
-    let monitor = new worker.Worker(__dirname + "/monitor.js", {
-        workerData: {
-            path: server,
-            host: RequestUrl.toString(),
-            retry: 3,
-            delay: 5 * 60 * 1000
-        }
-    })
-    monitors[server] = monitor
-    monitor.on('message', function WorkerMessageHandler(value) {
-        switch (value.type) {
-            case constants.REQUEST_RETRY:
-                console.log("[Warn]Worker-%d:cannot got server list.retry %d/%d", monitor.threadId, value.data.now, value.data.total)
-                break
-            case constants.NORTHSTAR_RESPAWN:
-                console.log("[Info]Worker-%d:northstar invalid,reloading", monitor.threadId)
-                break;
-            case constants.FAILED_AFTER_RETRY:
-                console.log("[Error]Worker-%d:cannot got server list after %d times retry", monitor.threadId, value.data.retrys)
-                break
-            case constants.NORTHSTAR_CONSOLE_CLOSED:
-                console.log("[Info]Worker-%d:Northstar server \"%s\" exited,reloading", monitor.threadId, value.data.name)
-        }
-    })
-    monitor.on('error', function WorkerErrorHandler(err) {
-        console.log("[Error]Worker-%d:%s", this.threadId, err)
-        this.terminate()
-        if (retry <= 0) {
-            delete monitors[server]
-            return;
-        }
-        console.log("Worker Retry:%s", server)
-        CreateWorker(server, retry - 1)
-    })
-    monitor.on('exit', function WorkerExitHandler() {
-        delete monitors[server]
-    })
+for(let server of ServerPaths){
+    servers.push(new Server(server));
 }
-
-if (worker.isMainThread) {
-    for(let server of ServerPaths){
-        CreateWorker(server,3)
+for(let server of servers){
+    server.Start();
+}
+const checker=new Checker();
+checker.on('update',list=>{
+    for(let server of servers){
+        if(!Object.keys(list).includes(server.GetName())){
+            server.Restart();
+        }
     }
-}
-
-exports = module.exports = constants
+});
